393d2fbfda3f4618ba44a8c46ead6cfdaa5c8285
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-05-04 21:22:54 -0300

    consumindo API e exibindo os filmes dinamicamente

--- main.js
+++ main.js
@@ -1,66 +1,62 @@
-const movies = [
-    { 
-        cover: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/i9XdxHsFrcqLkRWSF1coOHo4R39.jpg",
-        title: "Super Mario Bros.: O Filme",
-        rating: 9.8,
-        year: 2023,
-        description: "Mario é um encanador junto com seu irmão Luigi. Um dia, eles vão parar no reino dos cogumelos, governado pela Princesa Peach, mas ameaçado pelo rei dos Koopas, que faz de tudo para conseguir reinar em todos os lugares.",
-        isFavorite: true
-    },
-    { 
-        cover: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/nnryxxmgS17IBKvu23Hpg1mimZU.jpg",
-        title: "Renfield - Dando Sangue pelo Chefe",
-        rating: 8.8,
-        year: 2023,
-        description: "Renfield, o sofrido ajudante do chefe mais narcisista da história, Drácula, é forçado a encontrar as vítimas para seu mestre e fazer tudo o que ele lhe pede, qualquer que seja o grau de degradação da ordem recebida. Mas agora, depois de séculos de servidão, Renfield está pronto para descobrir se há vida lá fora, para além da sombra do Príncipe das Trevas. Se pelo menos ele puder descobrir como dar fim à sua dependência dele.",
-        isFavorite: true
-    },
-    { 
-        cover: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bS6g13i3LByRPDcuA7rwGIqSoKT.jpg",
-        title: "Três Homens em Conflito",
-        rating: 10,
-        year: 1966,
-        description: "Durante o auge da Guerra Civil, um misterioso pistoleiro vaga pela fronteira do oeste. Ele não possui um lar, lealdade ou companhia... Até que encontra dois estrangeiros, que são tão brutos e desapegados quanto ele. Unidos pelo destino, os três homens juntam suas forças para tentar encontrar uma fortuna em ouro roubado. Mas trabalho em equipe não é uma coisa natural para voluntariosos pistoleiros, e eles logo descobrem que seu maior desafio é concentrar-se em sua perigosa missão — e em manterem-se vivos — atravessando um país arrasado pela guerra.",
-        isFavorite: false
-    },
-    { 
-        cover: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/r3pPehX4ik8NLYPpbDRAh0YRtMb.jpg",
-        title: "Clube da Luta",
-        rating: 10,
-        year: 1999,
-        description: "Um homem deprimido que sofre de insônia conhece um estranho vendedor de sabonetes chamado Tyler Durden. Eles formam um clube clandestino com regras rígidas onde lutam com outros homens cansados de suas vidas mundanas. Mas sua parceria perfeita é comprometida quando Marla chama a atenção de Tyler.",
-        isFavorite: true
-    }
-];
+import { apiKey } from "./apiKey.js";
 
 const movieList = document.getElementById("movieList");
+
+async function getPopularMovies() {
+    try {
+        let request =  await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`);
+        let resposta = await request.json();
+        let movies = resposta.results;
+        
+        movies.forEach((movie) => {
+            console.log(movie)
+            renderMovies(movie);
+        })
+    }
+
+    catch(error) {
+        console.log(error);
+    }
+}
 
 function isFavorite(movie) {
     if (movie.isFavorite) {
         return `
         <img src="assets/heart-fill-icon.svg">
         <span>Desfavoritar</span>
-        `
+        `;
     } else {
         return `
         <img src="assets/heart-icon.svg">
         <span>Favoritar</span>
-        `
+        `;
     }
 }
 
+// "formatar" a visualização da data pra aparecer só o ano
+function formatReleaseYear(date) {
+    let year = date.split("-")[0];
+    return year;
+}
+
+function showDescription(description) {
+    return description == "" ? "Sinopse não disponível." : description;
+}
+
 function renderMovies(movie) {
+    let imagePath = "https://image.tmdb.org/t/p/w500/";
+
     movieList.innerHTML +=  `
     <div class="movie_item">
         <div class="movie_image">
-            <img src="${movie.cover}" alt="">
+            <img src="${imagePath}${movie.backdrop_path}" alt="">
         </div>
         <div class="movie_infoBox">
-            <p class="movie_title">${movie.title} (${movie.year})</p>
+            <p class="movie_title">${movie.title} (${ formatReleaseYear(movie.release_date) })</p>
             <div class="movie_actions">
                 <div class="movie_actionBox">
                     <img src="assets/star-icon.svg">
-                    <span id="movieRating">${movie.rating}</span>
+                    <span id="movieRating">${movie.vote_average}</span>
                 </div>
                 <div class="movie_actionBox">
                     ${ isFavorite(movie) }
@@ -68,12 +64,11 @@ function renderMovies(movie) {
             </div>
         </div>
         <div class="movie_description">
-            <p>${movie.description}</p>
+            <p>${ showDescription(movie.overview) }</p>
         </div>
     </div>
     `
 }
 
-movies.forEach((movie) => {
-    renderMovies(movie);
-})
+// consume a api toda vez que a janela é carregada
+window.addEventListener("load", getPopularMovies);
