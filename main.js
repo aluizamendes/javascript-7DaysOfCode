9bab3065b2694b9c6b07a2b7a9cde9ae5fb752b9
Ana Luiza Mendes <101937119+aluizamendes@users.noreply.github.com>
2023-05-05 20:02:37 -0300

    exibir filme pesquisado

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 import { apiKey } from "./apiKey.js";
 
 const movieList = document.getElementById("movieList");
+const inputPesquisar = document.getElementById("pesquisar");
 
 async function getPopularMovies() {
+    let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`;
+    
     try {
-        let request =  await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=pt-BR&page=1`);
-        let resposta = await request.json();
-        let movies = resposta.results;
-        
-        movies.forEach((movie) => {
-            console.log(movie)
-            renderMovies(movie);
-        })
+        let request =  await fetch(url);
+
+        // desestruturação de array
+        let { results } = await request.json();
+        return results;
     }
 
     catch(error) {
         console.log(error);
+    }
+}
+
+async function getSearchedMovie(movieName) {
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=pt-BR&query=${movieName}&page=1&include_adult=false`;
+
+    try {
+        let request = await fetch(url);
+        let { results } = await request.json();
+        return results;
+    }
+
+    catch(error) {
+        console.error(`Erro ao carregar recursos: ${error}`);
     }
 }
 
@@ -43,13 +57,18 @@ function showDescription(description) {
     return description == "" ? "Sinopse não disponível." : description;
 }
 
+async function showPopularMovies() {    
+    const movies = await getPopularMovies();
+    movies.forEach(movie => renderMovies(movie));
+}
+
 function renderMovies(movie) {
     let imagePath = "https://image.tmdb.org/t/p/w500/";
 
     movieList.innerHTML +=  `
     <div class="movie_item">
         <div class="movie_image">
-            <img src="${imagePath}${movie.backdrop_path}" alt="">
+            <img src="${imagePath}${movie.poster_path}" alt="">
         </div>
         <div class="movie_infoBox">
             <p class="movie_title">${movie.title} (${ formatReleaseYear(movie.release_date) })</p>
@@ -71,4 +90,32 @@ function renderMovies(movie) {
 }
 
 // consume a api toda vez que a janela é carregada
-window.addEventListener("load", getPopularMovies);
+window.addEventListener("load", async () => {
+    await showPopularMovies();
+});
+
+// pesquisar e exibir filme
+// se o  input estiver vazio e o user der `enter` ou `focusout` volta a mostrar os filmes populares
+
+inputPesquisar.addEventListener("keyup", async (evt) => { 
+
+    if (evt.key == "Enter") {
+        let searchedMovies = await getSearchedMovie(inputPesquisar.value);
+
+        movieList.innerHTML = "";
+        searchedMovies.forEach(movie => renderMovies(movie));
+
+        if (inputPesquisar.value == "") {
+            movieList.innerHTML = "";
+            await showPopularMovies();
+        }
+    } 
+})
+
+inputPesquisar.addEventListener("focusout", async () => {
+    
+    if (inputPesquisar.value == "") {
+        movieList.innerHTML = "";
+        await showPopularMovies();  
+    }
+})
